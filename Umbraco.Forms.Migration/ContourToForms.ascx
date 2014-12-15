df31d5a892ed6e013fba9eea6899cd49faf4994d
TimGeyssens <tg@umbraco.com>
2014-12-15 10:33:58 +0100

    Cleanup of the migration control

--- Umbraco.Forms.Migration/ContourToForms.ascx
+++ Umbraco.Forms.Migration/ContourToForms.ascx
@@ -1,5 +1,7 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ContourToForms.ascx.cs" Inherits="Umbraco.Forms.Migration.ContourToForms" %>
-<asp:TextBox ID="TextBox1" runat="server" Height="112px" TextMode="MultiLine" Width="250px" placeholder="Connectionstring"></asp:TextBox>
+<h4>Umbraco Forms migration</h4>
+<p>This tool will help you migrate Contour forms and workflows from a Contour instance to an Umbraco Forms instance, simply provide a connectionstring and hit the start migrate button.</p>
+<asp:TextBox ID="TextBox1" runat="server" Height="112px" TextMode="MultiLine" Width="250px" placeholder="server=SERVER;database=DB;user id=USER;password=PW"></asp:TextBox>
 <br />
 <br/>
 <asp:Button ID="Button1" runat="server" Text="Start Migration" OnClick="Button1_Click" />
--- Umbraco.Forms.Migration/ContourToForms.ascx.cs
+++ Umbraco.Forms.Migration/ContourToForms.ascx.cs
@@ -16,16 +16,20 @@ namespace Umbraco.Forms.Migration
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            var success = true;
             try
             {
                 var s = new MigrationService();
                 s.Migrate(TextBox1.Text);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                Literal1.Text = ex.Message + "<br/>" + ex.StackTrace;
+                success = false;
+                Literal1.Text = "<h4>Oops something whent wrong</h4><h5>" + ex.Message + "</h5><p>" + ex.StackTrace +"</p>";
             }
-            Literal1.Text = "Migration completed";
+            if(success)
+                Literal1.Text = "<h4>Migration completed<h4><p>Your forms have been migrated from the Contour instance.</p>";
+            
         }
     }
 }
